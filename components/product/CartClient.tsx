b00a32d14382f1ba8bc06d69ef30258af6af62d1
Nati Peretz <nati.peretz1985@gmail.com>
2024-02-22 18:38:34 +0200

    try again

--- components/product/CartClient.tsx
+++ components/product/CartClient.tsx
@@ -1,9 +1,23 @@
 "use client";
 
+import { ShoppingBag } from "lucide-react";
+
 const CartClient = ({ currentUser }: any) => {
   console.log({ currentUser });
 
-  return <div>CartClient</div>;
+  return (
+    <div className="mx-auto mb-20 max-w-7xl px-4 md:mb-40 md:px-8">
+      <div className="flex items-center justify-center gap-4 mb-6">
+        <span className="bg-teal-50 flex h-12 w-12 items-center justify-center rounded-full">
+          <div>
+            <ShoppingBag />
+          </div>
+        </span>
+        <h1 className="text-xl md:text-4xl font-bold">Cart</h1>
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default CartClient;
